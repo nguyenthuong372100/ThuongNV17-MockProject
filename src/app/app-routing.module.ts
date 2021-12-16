6b4a2f81d7a84f85f7ecb2cdd12998843c890fa1
hqb282k <hqb282k@gmail.com>
2021-12-16 14:03:08 +0700

    giao fien lan 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TotalComponent } from './total/total.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'world',component:TotalComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 
+import { TotalComponent } from './total/total.component';
+import { RegionComponent } from './region/region.component';
+import { HeaderComponent } from './header/header.component';
+import{ HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TotalComponent,
+    RegionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
